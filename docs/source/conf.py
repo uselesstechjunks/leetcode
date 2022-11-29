56e1fac063406fe42f40955ddc32790e6e79ab37
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2022-11-29 23:53:01 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SelfStudyGuide'
+copyright = '2022-2023, UselessTechJunks'
+author = 'Useless Tech Junks'
 
 release = '0.1'
 version = '0.1.0'
