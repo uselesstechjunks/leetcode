a5104600c40bde6f24544870031b9ba58db22bd5
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2023-01-05 18:41:57 +0530

    Update mergeintervals.cpp

--- docs/source/code/mergeintervals.cpp
+++ docs/source/code/mergeintervals.cpp
@@ -1,11 +1,11 @@
 vector<vector<int>> merge(vector<vector<int>>& intervals) {
 	auto cmp = [](const vector<int>& i1, const vector<int>& i2)
 	{
-	    if (i1[0] == i2[0])
-	    {
-				return i1[1] < i2[1];
-	    }
-	    return i1[0] < i2[0];
+		if (i1[0] == i2[0])
+		{
+			return i1[1] < i2[1];
+		}
+		return i1[0] < i2[0];
 	};
 
 	sort(intervals.begin(), intervals.end(), cmp);
@@ -14,18 +14,18 @@ vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
 	for (size_t i = 1; i < intervals.size(); ++i)
 	{
-	    vector<int>& last = ret.back();
-	    const vector<int>& curr = intervals[i];
+		vector<int>& last = ret.back();
+		const vector<int>& curr = intervals[i];
 
-	    if (last[1] >= curr[0])
-	    {
-				last[0] = min(last[0], curr[0]);
-				last[1] = max(last[1], curr[1]);
-	    }
-	    else
-	    {
-				ret.push_back(curr);
-	    }
+		if (last[1] >= curr[0])
+		{
+			last[0] = min(last[0], curr[0]);
+			last[1] = max(last[1], curr[1]);
+		}
+		else
+		{
+			ret.push_back(curr);
+		}
 	}
 
 	return ret;
