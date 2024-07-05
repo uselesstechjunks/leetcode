4d2d7cfd754cd17e15e3f3529a0aeac55de8368d
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2024-07-06 03:52:43 +0530

    Update multiheadattn.py

--- docs/source/code/multiheadattn.py
+++ docs/source/code/multiheadattn.py
@@ -1,48 +1,46 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+
+def mh_attn(q,K,V):
+    """
+    args:
+        q: [h,k]
+        K: [h,m,k]
+        V: [h,m,v]
+    returns:
+        o: [h,v]
+    """
+    logits = torch.einsum('hk,hmk->hm',q,K)
+    weights = F.softmax(logits, dim=1)
+    o = torch.einsum('hm,hmv->hv',weights, V)
+    return o
 
 class MultiHeadAttention(torch.nn.Module):
     def __init__(self, h, d, k, v):
         super(MultiHeadAttention, self).__init__()
-        self.Wq = torch.nn.Parameter(torch.randn(h, d, k))
-        self.Wk = torch.nn.Parameter(torch.randn(h, d, k))
-        self.Wv = torch.nn.Parameter(torch.randn(h, d, v))
-        self.Wo = torch.nn.Parameter(torch.randn(h, d, v))
+        self.Wq = nn.Parameter(torch.randn(h,d,k))
+        self.Wk = nn.Parameter(torch.randn(h,d,k))
+        self.Wv = nn.Parameter(torch.randn(h,d,v))
+        self.Wo = nn.Parameter(torch.randn(h,v,d))
 
     def forward(self, x, M):
-         return self.multi_head_attn(x, M, self.Wq, self.Wk, self.Wv, self.Wo)    
-
-    def multi_head_attn(self, x, M, Wq, Wk, Wv, Wo):
-        """
-        args:
-            x: vector with shape [d]
-            M: matrix with shape [m,d]
-            Wq: matrix with shape [h,d,k]
-            Wk: matrix with shape [h,d,k]
-            Wv: matrix with shape [h,d,v]
-            Wo: matrix with shape [h,d,v]
-        returns:
-            y: vector with shape [v]
-        """
-        q = torch.einsum('d,hdk->hk', x, Wq)
-        K = torch.einsum('md,hdk->hmk', M, Wk)
-        V = torch.einsum('md,hdv->hmv', M, Wv)
-        logits = torch.einsum('hk,hmk->hm', q, K)
-        weights = F.softmax(logits,dim=1)
-        o = torch.einsum('hm,hmv->hv', weights, V)
-        y = torch.einsum('hv,hdv->d', o, Wo)
+        q = torch.einsum('d,hdk->hk', x, self.Wq)
+        K = torch.einsum('md,hdk->hmk', M, self.Wk)
+        V = torch.einsum('md,hdv->hmv', M, self.Wv)
+        o = mh_attn(q, K, V)
+        y = torch.einsum('hv,hvd->d', o, self.Wo)
         return y
 
-
 if __name__ == '__main__':
-    d = 6
+    d = 16
     k = 8
-    v = 4
     m = 10
-    h = 32
+    v = 8
+    h = 2
 
-    M = torch.randn(m,d)
+    M = torch.randn((m,d))
 
-    attn = MultiHeadAttention(h, d, k, v)
-    y = attn(M[0], M)
+    model = MultiHeadAttention(h,d,k,v)
+    y = model(M[0],M)
     print(y)
