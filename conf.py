d2dcb28afd9370d7afb1ca7f6e4b34eab5c86e38
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2024-07-06 00:17:01 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,7 @@
 extensions = ['sphinx_rtd_dark_mode',
+              'sphinx_rtd_theme',
               'sphinx.ext.autosectionlabel']
+
+html_theme = 'sphinx_rtd_dark_mode'
 # user starts in dark mode
 default_dark_mode = True
