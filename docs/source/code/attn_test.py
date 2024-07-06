e0b8ee335117db7e532a74344b42a298a9379c34
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2024-07-07 04:02:35 +0530

    Update attn_test.py

--- docs/source/code/attn_test.py
+++ docs/source/code/attn_test.py
@@ -1,43 +1,49 @@
 """
 Sample output:
 --------------------------------------
-tensor([ -3.0437,  -5.3354,  -2.7996,   4.7690,   6.1953,  -3.1872,   2.4339,
-         -4.9126,  -0.5149,  -3.6056,   1.6128, -14.4580,  -2.2639,  -2.7896,
-         -0.7055,   6.9216], grad_fn=<ViewBackward0>)
-The following outputs should be the exact same with fixed seed.
+tensor([-6.1852,  4.9277, -7.1767, -5.7050,  3.5045,  1.6870,  5.5653, -2.3704,
+         2.2934,  1.3846,  2.3040, -5.1264,  2.2573, -1.0059, -3.2234, -0.6346],
+       grad_fn=<ViewBackward0>)
 MultiHeadAttention
-tensor([-18.4005,  11.4970,   5.9840,   9.8845, -15.3181,   1.3615,  -2.5959,
-         17.3029, -11.3590,  25.8750, -14.3187,  -3.3374,   2.2135, -13.3058,
-         -1.9368, -15.8990], grad_fn=<ViewBackward0>)
+tensor([ -1.9919, -18.8973, -11.5190,  -3.5315, -25.3034, -26.6213,  26.3794,
+        -19.0316, -11.3692, -25.6988,  -5.5753,  11.8805,   2.4946,  27.0999,
+         -5.6713,   4.9562], grad_fn=<ViewBackward0>)
 MultiHeadAttentionSequential
-tensor([-18.4005,  11.4970,   5.9840,   9.8845, -15.3181,   1.3615,  -2.5959,
-         17.3029, -11.3590,  25.8750, -14.3187,  -3.3374,   2.2135, -13.3058,
-         -1.9368, -15.8990], grad_fn=<ViewBackward0>)
+tensor([ -1.9919, -18.8973, -11.5190,  -3.5315, -25.3034, -26.6213,  26.3794,
+        -19.0316, -11.3692, -25.6988,  -5.5753,  11.8805,   2.4946,  27.0999,
+         -5.6713,   4.9562], grad_fn=<ViewBackward0>)
 MaskedMultiHeadAttentionParallel
-tensor([-18.4005,  11.4970,   5.9840,   9.8845, -15.3181,   1.3615,  -2.5959,
-         17.3029, -11.3590,  25.8750, -14.3187,  -3.3374,   2.2135, -13.3058,
-         -1.9368, -15.8990], requires_grad=True)
-MaskedMultiHeadAttentionParallelBatched
-tensor([-18.4005,  11.4970,   5.9840,   9.8845, -15.3181,   1.3615,  -2.5959,
-         17.3029, -11.3590,  25.8750, -14.3187,  -3.3374,   2.2135, -13.3058,
-         -1.9368, -15.8990], requires_grad=True)
+tensor([ -1.9919, -18.8973, -11.5190,  -3.5315, -25.3034, -26.6213,  26.3794,
+        -19.0316, -11.3692, -25.6988,  -5.5753,  11.8805,   2.4946,  27.0999,
+         -5.6712,   4.9562], requires_grad=True)
 MultiHeadAttentionSequentialBatched
-tensor([-18.4005,  11.4970,   5.9840,   9.8845, -15.3181,   1.3615,  -2.5959,
-         17.3029, -11.3590,  25.8750, -14.3187,  -3.3374,   2.2135, -13.3058,
-         -1.9368, -15.8990], requires_grad=True)
+tensor([ -1.9919, -18.8973, -11.5190,  -3.5315, -25.3034, -26.6213,  26.3794,
+        -19.0316, -11.3692, -25.6988,  -5.5753,  11.8805,   2.4946,  27.0999,
+         -5.6713,   4.9562], requires_grad=True)
+MaskedMultiHeadAttentionParallelBatched
+tensor([ -1.9919, -18.8973, -11.5190,  -3.5315, -25.3034, -26.6213,  26.3794,
+        -19.0316, -11.3692, -25.6988,  -5.5753,  11.8805,   2.4946,  27.0999,
+         -5.6712,   4.9562], requires_grad=True)
+MultiQueryAttentionSequentialBatched
+tensor([  1.2325, -17.5478,   2.6610,  19.4804,   1.7672, -54.5261, -12.6656,
+         -4.2605,   3.6807,   3.2058,  29.4352, -23.4512,   9.5014,  22.9171,
+         50.5439,  11.1721], requires_grad=True)
+MaskedMultiQueryAttentionParallelBatched
+tensor([  1.2325, -17.5478,   2.6610,  19.4804,   1.7672, -54.5261, -12.6656,
+         -4.2605,   3.6807,   3.2058,  29.4352, -23.4512,   9.5014,  22.9171,
+         50.5439,  11.1721], requires_grad=True)
 """
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 def attn(q,K,V):
     """
-    args:
+    Args:
         q: [k]
         K: [m,k]
         V: [m,v]
-    returns:
+    Returns:
         y: [v]
     """
     logits = torch.einsum('k,mk->m',q,K)
@@ -47,11 +53,11 @@ def attn(q,K,V):
 
 def mha(q,K,V):
     """
-    args:
+    Args:
         q: [h,k]
         K: [h,m,k]
         V: [h,m,v]
-    returns:
+    Returns:
         o: [h,v]
     """
     logits = torch.einsum('hk,hmk->hm',q,K)
@@ -61,12 +67,12 @@ def mha(q,K,V):
 
 def mha_par(Q,K,V,mask):
     """
-    args:
+    Args:
         Q: [h,n,k]
         K: [h,m,k]
         V: [h,m,v]
         mask: [n,m]
-    returns:
+    Returns:
         O: [h,n,v]
     """
     logits = torch.einsum('hnk,hmk->hnm',Q,K)
@@ -78,11 +84,11 @@ def mha_par(Q,K,V,mask):
 
 def mha_batched(q,K,V):
     """
-    args:
+    Args:
         q: [b,h,k]
         K: [b,h,m,k]
         V: [b,h,m,v]
-    returns:
+    Returns:
         O: [b,h,v]
     """
     logits = torch.einsum('bhk,bhmk->bhm',q,K)
@@ -90,14 +96,28 @@ def mha_batched(q,K,V):
     o = torch.einsum('bhm,bhmv->bhv', weights, V)
     return o
 
+def mqa_batched(q,K,V):
+    """
+    Args:
+        q: [b,h,k]
+        k: [b,m,k]
+        v: [b,m,v]
+    Returns:
+        o: [b,h,n,v]
+    """
+    logits = torch.einsum('bhk,bmk->bhm', q, K)
+    weights = F.softmax(logits, dim=-1)
+    o = torch.einsum('bhm,bmv->bhv', weights, V)
+    return o
+
 def mha_par_batched(Q,K,V,mask):
     """
-    args:
+    Args:
         Q: [b,h,n,k]
         K: [b,h,m,k]
         V: [b,h,m,v]
         mask: [n,m]
-    returns:
+    Returns:
         O: [b,h,n,v]
     """
     logits = torch.einsum('bhnk,bhmk->bhnm', Q, K)
@@ -105,6 +125,23 @@ def mha_par_batched(Q,K,V,mask):
     logits.masked_fill_(attn_mask, float('-inf'))
     weights = F.softmax(logits, dim=-1)
     O = torch.einsum('bhnm,bhmv->bhnv', weights, V)
+    return O
+
+def mqa_par_batched(Q,K,V,mask):
+    """
+    Args:
+        Q: [b,h,n,k]
+        K: [b,m,k]
+        V: [b,m,v]
+        mask: [n,m]
+    Returns:
+        O: [b,h,n,v]
+    """
+    logits = torch.einsum('bhnk,bmk->bhnm', Q, K)
+    attn_mask = mask == 0
+    logits.masked_fill_(attn_mask, float('-inf'))
+    weights = F.softmax(logits, dim=-1)
+    O = torch.einsum('bhnm,bmv->bhnv', weights, V)
     return O
 
 class Attention(torch.nn.Module):
@@ -177,17 +214,17 @@ class MultiHeadAttentionSequential(torch.nn.Module):
         self.Wv = nn.Parameter(torch.randn(h,d,v))
         self.Wo = nn.Parameter(torch.randn(h,v,d))
 
-    '''
-    args:
-        x: [d]
-        prev_K: [h,m,k]
-        prev_V: [h,m,v]
-    returns:
-        y: [d]
-        K: [h,m+1,k]
-        V: [h,m+1,v]
-    '''
     def forward(self, x, prev_K, prev_V):
+        """
+        Args:
+            x: [d]
+            prev_K: [h,m,k]
+            prev_V: [h,m,v]
+        Returns:
+            y: [d]
+            K: [h,m+1,k]
+            V: [h,m+1,v]
+        """
         q = torch.einsum('d,hdk->hk', x, self.Wq)
         K = torch.cat((prev_K, torch.einsum('d,hdk->hk', x, self.Wk).unsqueeze(-2)), dim=-2)
         V = torch.cat((prev_V, torch.einsum('d,hdv->hv', x, self.Wv).unsqueeze(-2)), dim=-2)
@@ -203,17 +240,17 @@ class MultiHeadAttentionSequentialBatched(torch.nn.Module):
         self.Wv = nn.Parameter(torch.randn(h,d,v))
         self.Wo = nn.Parameter(torch.randn(h,v,d))
 
-    '''
-    args:
-        x: [b,d]
-        prev_K: [b,h,m,k]
-        prev_V: [b,h,m,v]
-    returns:
-        y: [b,d]
-        K: [b,h,m+1,k]
-        V: [b,h,m+1,v]
-    '''
     def forward(self, x, prev_K, prev_V):
+        """
+        Args:
+            x: [b,d]
+            prev_K: [b,h,m,k]
+            prev_V: [b,h,m,v]
+        Returns:
+            y: [b,d]
+            K: [b,h,m+1,k]
+            V: [b,h,m+1,v]
+        """
         q = torch.einsum('bd,hdk->bhk', x, self.Wq)
         K = torch.cat((prev_K, torch.einsum('bd,hdk->bhk', x, self.Wk).unsqueeze(-2)), dim=-2)
         V = torch.cat((prev_V, torch.einsum('bd,hdv->bhv', x, self.Wv).unsqueeze(-2)), dim=-2)
@@ -221,27 +258,64 @@ class MultiHeadAttentionSequentialBatched(torch.nn.Module):
         y = torch.einsum('bhv,hvd->bd', o, self.Wo)
         return y, K, V
 
-if __name__ == '__main__':
-    d = 16
-    k = 8
-    m = 10
-    v = 8
-    h = 2
+class MaskedMultiQueryAttentionParallelBatched(torch.nn.Module):
+    def __init__(self, h, d, k, v):
+        super(MaskedMultiQueryAttentionParallelBatched, self).__init__()
+        self.Wq = nn.Parameter(torch.randn(h,d,k))
+        self.Wk = nn.Parameter(torch.randn(d,k))
+        self.Wv = nn.Parameter(torch.randn(d,v))
+        self.Wo = nn.Parameter(torch.randn(h,v,d))
 
-    M = torch.randn((m,d))
+    def forward(self, X, M, mask):
+        """
+        Args:
+            X: [b,n,d]
+            M: [b,m,d]
+            mask: [n,m]
+        Returns:
+            Y: [b,n,d]
+        """
+        Q = torch.einsum('bnd,hdk->bhnk', X, self.Wq)
+        K = torch.einsum('bmd,dk->bmk', M, self.Wk)
+        V = torch.einsum('bmd,dv->bmv', M, self.Wv)
+        O = mqa_par_batched(Q,K,V,mask)
+        Y = torch.einsum('bhnv,hvd->bnd', O, self.Wo)
+        return Y
 
-    model = Attention(d,k,d)
-    y = model(M[0],M)
-    print(y)
+class MultiQueryAttentionSequentialBatched(torch.nn.Module):
+    def __init__(self, h, d, k, v):
+        super(MultiQueryAttentionSequentialBatched, self).__init__()
+        self.Wq = nn.Parameter(torch.randn(h,d,k))
+        self.Wk = nn.Parameter(torch.randn(d,k))
+        self.Wv = nn.Parameter(torch.randn(d,v))
+        self.Wo = nn.Parameter(torch.randn(h,v,d))
 
-    print('The following outputs should be the exact same with fixed seed.')
+    def forward(self, x, prev_K, prev_V):
+        """
+        Args:
+            x: [b,d]
+            prev_K: [b,m,k]
+            prev_V: [b,m,v]
+        Returns:
+            y: [b,d]
+            K: [b,m+1,k]
+            V: [b,m+1,v]
+        """
+        q = torch.einsum('bd,hdk->bhk', x, self.Wq)
+        K = torch.cat((prev_K, torch.einsum('bd,dk->bk', x, self.Wk).unsqueeze(-2)), dim=-2)
+        V = torch.cat((prev_V, torch.einsum('bd,dv->bv', x, self.Wv).unsqueeze(-2)), dim=-2)
+        o = mqa_batched(q, K, V)
+        y = torch.einsum('bhv,hvd->bd', o, self.Wo)
+        return y, K, V
 
+def test_mha(MultiHeadAttention, d, k, v, h, M):
     torch.manual_seed(42)
     model = MultiHeadAttention(h,d,k,v)
     y = model(M[-1],M)
     with torch.no_grad():
         print(f'MultiHeadAttention\n{y}')
 
+def test_mha_seq(MultiHeadAttentionSequential, d, k, v, h, M):
     torch.manual_seed(42)
     model = MultiHeadAttentionSequential(h,d,k,v)
     prev_K = torch.FloatTensor(h,0,k)
@@ -254,16 +328,14 @@ if __name__ == '__main__':
     with torch.no_grad():
         print(f'MultiHeadAttentionSequential\n{y}')
 
-    # triangular mask mimicing the decoder
-    # same code can be reused for encoder with all bits on
-    mask = torch.tril(torch.ones(M.shape[0],M.shape[0]))
-    
+def test_mha_par(MaskedMultiHeadAttentionParallel, d, k, v, h, M, mask):
     torch.manual_seed(42)
     model = MaskedMultiHeadAttentionParallel(h,d,k,v)
     Y = model(M,M,mask)
     with torch.no_grad():
         print(f'MaskedMultiHeadAttentionParallel\n{Y[-1]}')
 
+def test_mha_par_batched(MaskedMultiHeadAttentionParallelBatched, d, k, v, h, M, mask):
     torch.manual_seed(42)
     model = MaskedMultiHeadAttentionParallelBatched(h,d,k,v)
     X = M.unsqueeze(0)
@@ -272,15 +344,73 @@ if __name__ == '__main__':
         Y = Y.squeeze(0)
         print(f'MaskedMultiHeadAttentionParallelBatched\n{Y[-1]}')
 
+def test_mha_seq_batched(MultiHeadAttentionSequentialBatched, d, k, v, h, M):
     torch.manual_seed(42)
     model = MultiHeadAttentionSequentialBatched(h,d,k,v)
     prev_K = torch.FloatTensor(1,h,0,k)
     prev_V = torch.FloatTensor(1,h,0,v)
-    Y = None
+    y = None
 
     for x in M:
-        Y, prev_K, prev_V = model(x.unsqueeze(0), prev_K, prev_V)
+        y, prev_K, prev_V = model(x.unsqueeze(0), prev_K, prev_V)
 
     with torch.no_grad():
-        y = Y.squeeze(0)
+        y = y.squeeze(0)
         print(f'MultiHeadAttentionSequentialBatched\n{y}')
+
+def test_mqa_par_batched(MaskedMultiQueryAttentionParallelBatched, d, k, v, h, M, mask):
+    torch.manual_seed(42)
+    model = MaskedMultiQueryAttentionParallelBatched(h,d,k,v)
+    X = M.unsqueeze(0)
+    Y = model(X,X,mask)
+    with torch.no_grad():
+        Y = Y.squeeze(0)
+        print(f'MaskedMultiQueryAttentionParallelBatched\n{Y[-1]}')
+
+def test_mqa_seq_batched(MultiQueryAttentionSequentialBatched, d, k, v, h, M):
+    torch.manual_seed(42)
+    model = MultiQueryAttentionSequentialBatched(h,d,k,v)
+    prev_K = torch.FloatTensor(1,0,k)
+    prev_V = torch.FloatTensor(1,0,v)
+    y = None
+
+    for x in M:
+        y, prev_K, prev_V = model(x.unsqueeze(0), prev_K, prev_V)
+
+    with torch.no_grad():
+        y = y.squeeze(0)
+        print(f'MultiQueryAttentionSequentialBatched\n{y}')
+
+if __name__ == '__main__':
+    d = 16
+    k = 8
+    m = 10
+    v = 8
+    h = 2
+
+    M = torch.randn((m,d))
+    # triangular mask mimicing the decoder
+    # same code can be reused for encoder with all bits on
+    mask = torch.tril(torch.ones(M.shape[0],M.shape[0]))
+
+    #########################################
+    # SHA
+    #########################################
+    model = Attention(d,k,d)
+    y = model(M[0],M)
+    print(y)
+
+    #########################################
+    # MHA
+    #########################################
+    test_mha(MultiHeadAttention, d, k, v, h, M)
+    test_mha_seq(MultiHeadAttentionSequential, d, k, v, h, M)
+    test_mha_par(MaskedMultiHeadAttentionParallel, d, k, v, h, M, mask)    
+    test_mha_seq_batched(MultiHeadAttentionSequentialBatched, d, k, v, h, M)
+    test_mha_par_batched(MaskedMultiHeadAttentionParallelBatched, d, k, v, h, M, mask)
+
+    #########################################
+    # MQA
+    #########################################    
+    test_mqa_seq_batched(MultiQueryAttentionSequentialBatched, d, k, v, h, M)
+    test_mqa_par_batched(MaskedMultiQueryAttentionParallelBatched, d, k, v, h, M, mask)
