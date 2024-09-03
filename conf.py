10b291a788f785c1fd7ef7deacea5a78e9e5c1ac
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2024-09-03 13:37:15 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,2 +1,6 @@
-extensions = ['sphinx_rtd_theme',
-              'sphinx.ext.autosectionlabel']
+from custom_directives import DefinitionsDirective
+
+extensions = ['sphinx_rtd_theme','sphinx.ext.autosectionlabel']
+
+def setup(app):
+	app.add_directive('definitions', DefinitionsDirective)
