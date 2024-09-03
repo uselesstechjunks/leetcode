12bbf39f21b1cdcbdaf8a8587636c34165637253
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2024-09-03 20:32:30 +0530

    Update vecspace.rst (#337)
    * Update vecspace.rst
    
    * Update findim.rst
    
    * Update findim.rst
    
    * Create custom_directives.py
    
    * Rename docs/custom_directives.py to custom_directives.py
    
    * Update conf.py
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update conf.py
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update findim.rst
    
    * Update vecspace.rst
    
    * Update vecspace.rst
    
    * Update themes.rst
    
    * Update themes.rst

--- conf.py
+++ conf.py
@@ -1,2 +1,11 @@
-extensions = ['sphinx_rtd_theme',
-              'sphinx.ext.autosectionlabel']
+from custom_directives import DefinitionsDirective
+
+extensions = ['sphinx_rtd_theme','sphinx.ext.autosectionlabel']
+
+'''
+Markdown helps:
+https://www.sphinx-doc.org/en/master/usage/restructuredtext/directives.html#directive-admonition
+'''
+
+def setup(app):
+	app.add_directive('definitions', DefinitionsDirective)
