2ff2824ef7a4cf0f23cace1d9476cd54fdf206d1
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2025-03-01 14:43:42 +0530

    Update binsearch_rot.py

--- docs/source/code/binsearch_rot.py
+++ docs/source/code/binsearch_rot.py
@@ -1,41 +1,68 @@
-def findMin(self, nums: List[int]) -> int:
-	"""
-		observations: 
-		1. base case: num[left] < num[right] -> left is the answer
-		2. at any given time, the range [left, right] contains the answer
-		3. to satisfy left should never cross right, the condition should allow left == right
-		4. in the end, the range reduces to 1 and left is the answer
-	"""
-	def check_with_right():
-		# search range: [0, n-1]
+def search(self, nums: List[int], target: int) -> int:
+	def direct_search():
+		# search range: 0, n-1
 		left, right = 0, len(nums) - 1
-		# loop ends when left == right or num[left] < num[right]
-		while left != right and nums[left] > nums[right]:
-			# for the following, num[left] > num[right]
-			# find mid
+		while left <= right:
 			mid = (left + right) // 2
-			# if num[mid] < num[right], answer lies in [left, mid]
-			if nums[mid] < nums[right]:
-				right = mid
-			# if num[mid] > num[right], answer lies in [mid + 1, right]
+			if nums[mid] == target:
+				return mid
+			# nums[left] <= nums[mid] -> min of array lies in [mid + 1, right]
+			if nums[left] <= nums[mid]:
+				if target < nums[mid]:
+					# there can be elements in both parts which are smaller than target
+					# if left itself is larger than target, we can eliminate left half
+					if nums[left] > target:
+						left = mid + 1
+					else:
+						right = mid - 1
+				# if target > nums[mid]:
+				else:
+					# target has to be on the right side
+					left = mid + 1
+			# nums[left] > nums[mid] -> min of array lies in [left, mid]
 			else:
-				left = mid + 1
-		# left contains the min
-		return nums[left]
-		
-	def check_with_left():
-		# search range: [0, n-1]
-		left, right = 0, len(nums) - 1
-		# loop ends when left == right or num[left] < num[right]
-		while left != right and nums[left] > nums[right]:
-			# for the following, num[left] > num[right]
-			# find mid
-			mid = (left + right) // 2
-			# if num[left] > num[mid], answer lies in [left, mid]
-			if nums[left] > nums[mid]:
-				right = mid
-			# if num[left] < num[mid], answer lies in [mid + 1, right]
-			else:
-				left = mid + 1
-		# left contains the min
-		return nums[left]
+				if target < nums[mid]:
+					# target has to be on the left side
+					right = mid - 1
+				# if target > nums[mid]:
+				else:
+					# there are elements in both parts which are larger than target
+					# if right itself is smaller than target, we can eliminate right half
+					if nums[right] < target:
+						right = mid - 1
+					else:
+						left = mid + 1
+		return -1
+
+	def indirect_search():
+		def find_pivot():
+			left, right = 0, len(nums) - 1
+			while left != right and nums[left] > nums[right]:
+				mid = (left + right) // 2
+				# if nums[left] > nums[mid]:
+				if nums[mid] < nums[right]:
+					right = mid
+				else:
+					left = mid + 1
+			return left
+
+		def search_with_pivot(pivot_index):
+			n = len(nums)
+			left, right = 0, n - 1
+			while left <= right:
+				mid = (left + right) // 2
+				pivot = (mid + pivot_index) % n
+				if target == nums[pivot]:
+					return pivot
+				if target < nums[pivot]:
+					right = mid - 1
+				else:
+					left = mid + 1
+			return -1
+
+		return search_with_pivot(find_pivot())
+
+	direct = direct_search()
+	indirect = indirect_search()
+
+	assert(direct == indirect)
