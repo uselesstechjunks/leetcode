3aa28a2c3098a4a139ab2163332bc53ffc9254e6
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2025-03-02 16:39:38 +0530

    Update tsort.py

--- docs/source/code/tsort.py
+++ docs/source/code/tsort.py
@@ -1,49 +1,69 @@
 from collections import deque
 
-class Solution:
-	def kahn(self, n, edges):
-        graph = [[] for _ in range(n)]
-        indeg = [0] * n
-        for u, v in edges:
-            graph[u].append(v)
-            indeg[v] += 1
-        queue = deque([u for u in range(n) if indeg[u] == 0])
-        discovered = [False] * n
-        order = []
-        while queue:
-            u = queue.popleft()            
-            order.append(u)
-            for v in graph[u]:
-                if discovered[v]: return [] # back edge, not a DAGs
-                indeg[v] -= 1
-                if indeg[v] == 0:
-                    discovered[v] = True
-                    queue.append(v)
-        return order if len(order) == n else []
+def kahn():
+	# won't work if there are cycles
+	def build(n, edges):
+		in_degrees = [0] * n
+		adj = [[] for _ in range(n)]
+		for a, b in edges:
+			adj[b].append(a)
+			in_degrees[a] += 1
+		return adj, in_degrees
+	
+	def bfs():
+		nonlocal adj, visited, queue, tsorted, in_degrees
+		while queue:
+			u = queue.popleft()
+			tsorted.append(u)
+			for v in adj[u]:
+				in_degrees[v] -= 1
+				if not in_degrees[v]:
+					queue.append(v)
 
-    def tarjan(self, n, edges):
-        def dfs(graph, discovered, processed, u, order):
-            discovered[u] = True
-            for v in graph[u]:
-                if not discovered[v]:
-                    dfs(graph, discovered, processed, v, order)
-                elif not processed[v]:
-                    return # back edge, not a DAG
-            processed[u] = True
-            order.insert(0, u)
-        graph = [[] for _ in range(n)]
-        for u, v in edges:
-            graph[u].append(v)
-        discovered = [False] * n
-        processed = [False] * n
-        order = []
-        for u in range(n):
-            if not discovered[u]:
-                dfs(graph, discovered, processed, u, order)
-        return order if len(order) == n else []
+	adj, in_degrees = build(numCourses, prerequisites)
+	visited = [False] * numCourses
 
-    def tsort(self, n, edges, algorithm='tarjan'):
-        if algorithm == 'tarjan':
-            return self.tarjan(n, edges)
-        if algorithm == 'kahn':
-            return self.kahn(n, edges)
+	queue = deque([])
+	for u in range(numCourses):
+		if not in_degrees[u]:
+			visited[u] = True
+			queue.append(u)
+
+	tsorted = []
+	if bfs():
+		return []
+	return tsorted if len(tsorted) == numCourses else []
+
+def tarjan():
+	# since this uses dfs, it can detect cycles
+	def build(n, edges):
+		adj = [[] for _ in range(n)]
+		for a, b in edges:
+			adj[b].append(a)
+		return adj
+
+	def has_cycle(u):
+		nonlocal visited, finished, adj, tsorted
+		visited[u] = True
+		for v in adj[u]:
+			if visited[v] and not finished[v]: # back edge
+				return True # cycle exists
+			if not visited[v] and has_cycle(v):
+				return True
+		finished[u] = True
+		tsorted.insert(0, u)
+		return False
+
+	adj = build(numCourses, prerequisites)
+	tsorted = []
+	visited = [False] * numCourses
+	finished = [False] * numCourses
+	# no need to find root edges in the dag
+	# dfs would figure it out
+	for u in range(numCourses):
+		if not visited[u]:
+			# dfs returns true if there is a cycle, in which case it's not a dag
+			# we return empty list
+			if has_cycle(u):
+				return []
+	return tsorted
