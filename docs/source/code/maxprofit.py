c74b9ccfe10b1e815cdc7ef81ae77ebcb3ad9907
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2025-03-08 20:43:31 +0530

    Update and rename buystockcooldown.py to maxprofit.py

--- docs/source/code/maxprofit.py
+++ docs/source/code/maxprofit.py
@@ -1,3 +1,4 @@
+# with cooldown
 def maxProfit(self, prices: List[int]) -> int:
 	"""
 	states: (a) not holding (b) holding (c) cooling down
@@ -23,4 +24,28 @@ def maxProfit(self, prices: List[int]) -> int:
 		# can come to cooling down state from holding state
 		dp[i][2] = dp[i-1][1] + prices[i]
 		print(i, prices[i], dp[i])
+	return max(dp[-1])
+
+# with at most 2 transactions
+def maxProfit(self, prices: List[int]) -> int:
+	"""
+	states:
+		1. no transactions
+		2. one transaction and holding
+		3. one transaction and not holding
+		4. two transactions and holding
+		5. two transactions and not holding
+	"""
+	n = len(prices)
+	""" NOTE -inf instead of 0 """
+	dp = [[-inf] * 5 for _ in range(n)]
+	dp[0][1] = -prices[0]
+	""" NOTE this initialization is required because of -inf """
+	dp[0][0] = 0
+	for i in range(1, n):
+		dp[i][0] = dp[i-1][0]
+		dp[i][1] = max(dp[i-1][0] - prices[i], dp[i-1][1])
+		dp[i][2] = max(dp[i-1][1] + prices[i], dp[i-1][2])
+		dp[i][3] = max(dp[i-1][2] - prices[i], dp[i-1][3])
+		dp[i][4] = max(dp[i-1][3] + prices[i], dp[i-1][4])
 	return max(dp[-1])
