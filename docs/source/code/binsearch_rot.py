330b8752e68bbce73750ec4067077c0f4c420230
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2025-03-08 09:55:41 +0530

    Update binsearch_rot.py

--- docs/source/code/binsearch_rot.py
+++ docs/source/code/binsearch_rot.py
@@ -1,109 +1,65 @@
-"""
-def search_without_duplicates(self, nums: List[int], target: int) -> int:
+def direct_search(self, nums: List[int], target: int) -> int:
 	left, right = 0, len(nums) - 1
 	while left <= right:
 		mid = (left + right) // 2
+		# mid cuts the array in 3 parts
+		# nums[left...mid-1], nums[mid], nums[mid+1...right]
+		# case 1: we can check the shortest part first
 		if nums[mid] == target:
 			return mid
-		if nums[left] <= nums[mid]:
+		"""
+		# case 2: of the two subarrays, at most one of them is rotated
+		# so at least one of them is sorted
+		"""
+		# NOTE: it's also possible that both of them are sorted (if mid was the min)
+		# we check if left part is the sorted one
+		if nums[left] <= nums[mid]: # FOR DUPLICATES: check for nums[left] < nums[mid]
+			""" left subarray is sorted """
+			# NOTE: the <= sign instead of < (even though numbers are distinct)
+			# this is to tackle cases of arrays of size 1 and 2
 			if nums[left] <= target and target < nums[mid]:
+				# we can check if target is contained in this sorted half
 				right = mid - 1
 			else:
+				# target is not in the sorted half
 				left = mid + 1
-		else: #if nums[left] > nums[mid]:
+		else: # FOR DUPLICATES: check for elif nums[left] > nums[mid]:
+			""" right subarray is sorted """
 			if nums[mid] < target and target <= nums[right]:
+				# we can check if target is contained in this sorted half
 				left = mid + 1
 			else:
+				# target is not in the sorted half
 				right = mid - 1
-		# else:
+		# FOR DUPLICATES, WE NEED TO RESORT TO LINEAR SEARCH
+		# else: 
 		#     left += 1
 	return -1
-def search_with_duplicates(self, nums: List[int], target: int) -> bool:
-	left, right = 0, len(nums) - 1
-	while left <= right:
-		mid = (left + right) // 2
-		if nums[mid] == target:
-			return True
-		if nums[left] < nums[mid]:
-			if nums[left] <= target and target < nums[mid]:
-				right = mid - 1
-			else:
-				left = mid + 1
-		elif nums[left] > nums[mid]:
-			if nums[mid] < target and target <= nums[right]:
-				left = mid + 1
-			else:
-				right = mid - 1
-		else:
-			left += 1
-	return False
-"""
 
-def search(self, nums: List[int], target: int) -> int:
-	def direct_search():
-		# search range: 0, n-1
+def indirect_search():
+	def find_pivot():
 		left, right = 0, len(nums) - 1
+		while left != right and nums[left] > nums[right]:
+			mid = (left + right) // 2
+			""" also works: if nums[left] > nums[mid]: """
+			if nums[mid] < nums[right]:
+				right = mid
+			else:
+				left = mid + 1
+		return left
+
+	def search_with_pivot(pivot_index):
+		n = len(nums)
+		left, right = 0, n - 1
 		while left <= right:
 			mid = (left + right) // 2
-			if nums[mid] == target:
-				return mid
-			# nums[left] <= nums[mid] -> min of array lies in [mid + 1, right]
-			if nums[left] <= nums[mid]:
-				if target < nums[mid]:
-					# there can be elements in both parts which are smaller than target
-					# if left itself is larger than target, we can eliminate left half
-					if nums[left] > target:
-						left = mid + 1
-					else:
-						right = mid - 1
-				# if target > nums[mid]:
-				else:
-					# target has to be on the right side
-					left = mid + 1
-			# nums[left] > nums[mid] -> min of array lies in [left, mid]
+			pivot = (mid + pivot_index) % n
+			if target == nums[pivot]:
+				return pivot
+			if target < nums[pivot]:
+				right = mid - 1
 			else:
-				if target < nums[mid]:
-					# target has to be on the left side
-					right = mid - 1
-				# if target > nums[mid]:
-				else:
-					# there are elements in both parts which are larger than target
-					# if right itself is smaller than target, we can eliminate right half
-					if nums[right] < target:
-						right = mid - 1
-					else:
-						left = mid + 1
+				left = mid + 1
 		return -1
 
-	def indirect_search():
-		def find_pivot():
-			left, right = 0, len(nums) - 1
-			while left != right and nums[left] > nums[right]:
-				mid = (left + right) // 2
-				# if nums[left] > nums[mid]:
-				if nums[mid] < nums[right]:
-					right = mid
-				else:
-					left = mid + 1
-			return left
-
-		def search_with_pivot(pivot_index):
-			n = len(nums)
-			left, right = 0, n - 1
-			while left <= right:
-				mid = (left + right) // 2
-				pivot = (mid + pivot_index) % n
-				if target == nums[pivot]:
-					return pivot
-				if target < nums[pivot]:
-					right = mid - 1
-				else:
-					left = mid + 1
-			return -1
-
-		return search_with_pivot(find_pivot())
-
-	direct = direct_search()
-	indirect = indirect_search()
-
-	assert(direct == indirect)
+	return search_with_pivot(find_pivot())
