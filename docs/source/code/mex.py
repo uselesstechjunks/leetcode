34282efba6e5d9252eeb6d6ebd607489a2df9c84
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2025-03-09 21:02:42 +0530

    Update mex.py

--- docs/source/code/mex.py
+++ docs/source/code/mex.py
@@ -1,4 +1,11 @@
 def withoutExtraSpace(self, nums) -> int:
+	def withExtraSpace(self, nums) -> int:
+		nums = set(nums)
+		mex = 1
+		while mex in nums:
+			mex += 1
+		return mex
+	
 	# use the array index itself as the set using index as lookup key
 	# traverse from left to right
 	# when we see a negative number, ignore
@@ -24,7 +31,7 @@ def withoutExtraSpace(self, nums) -> int:
 			nums[curr_index], nums[correct_index] = nums[correct_index], nums[curr_index]
 		elif correct_index < curr_index and correct_index >= 0:
 			nums[curr_index], nums[correct_index] = nums[correct_index], nums[curr_index]
-	print(nums)
+
 	index = 0
 	while index < length:
 		if nums[index] <= 0:
@@ -32,12 +39,65 @@ def withoutExtraSpace(self, nums) -> int:
 		index += 1
 	return length+1
 
-def withExtraSpace(self, nums) -> int:
-	nums = set(nums)
-	mex = 1
-	while mex in nums:
-		mex += 1
-	return mex
+	""" a dumber way of doing the above """
+	def constant_space():
+		"""
+		idea: use index as a proxy for the set above
+		"""
+		# NOTE
+		# 1. anyting that's negative doesn't matter
+		# 2. anything that's larger than the max index doesn't matter
+		# we need to ensure that we don't overwrite anything
+		# [0,3,4,-1,1]
+		# [0,3,4,-1,1] i = 1
+		# [0,0,4,3,1] i = 2
+		# [0,0,1,3,4] i = 2
+		numbers = [0] + nums
+		index = 1
+		while index < len(numbers):
+			if numbers[index] <= 0 or numbers[index] > len(nums):
+				numbers[index] = 0
+				index += 1
+			elif numbers[index] == index:
+				index += 1
+			elif numbers[numbers[index]] == numbers[index]:
+				numbers[index] = 0
+				index += 1
+			else:
+				tmp = numbers[numbers[index]]
+				numbers[numbers[index]] = numbers[index]
+				numbers[index] = tmp
+		
+		print(numbers)
+		curr = 1
+		while curr <= len(nums) and numbers[curr]:
+			curr += 1
+		return curr
 
 def firstMissingPositive(self, nums: List[int]) -> int:
 	return self.withoutExtraSpace(nums)
+
+# https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations/description/
+def findSmallestInteger(self, nums: List[int], value: int) -> int:
+	# NOTE this doesn't quite pass all testcases yet.
+	"""
+	The goal is to find maximum possible MEX, therefore we should
+	try to look at filling out as many as possible
+
+	the idea is to use modular arithmatic.
+
+	If we add value to any number x multiple times, we can transform
+	it to the range [0, value-1]. We can check x % value to find this number.
+
+	The problem then is to find MEX in [0, value-1] range.
+	"""
+	transformed = set()
+	for num in nums:
+		curr = num % value
+		while curr in transformed:
+			curr += value
+		transformed.add(curr)
+	curr = 0
+	while curr in transformed:
+		curr += 1
+	return curr
