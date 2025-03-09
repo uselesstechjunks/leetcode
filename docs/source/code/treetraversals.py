5edd2f45828d35765aa24a46d5901edd9ddeaae9
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2025-03-09 14:42:22 +0530

    Update treetraversals.py

--- docs/source/code/treetraversals.py
+++ docs/source/code/treetraversals.py
@@ -56,3 +56,34 @@ def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 	result = []
 	iter(root)
 	return result
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+	def __init__(self, root: Optional[TreeNode]):
+		self.stack = []
+		self.push_left(root)
+
+	def push_left(self, node):
+		while node:
+			self.stack.append(node)
+			node = node.left
+
+	def next(self) -> int:
+		top = self.stack.pop()
+		self.push_left(top.right)
+		return top.val
+
+	def hasNext(self) -> bool:
+		return len(self.stack) > 0
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
